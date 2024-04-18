c3fe5610f663406018e38edf28ff4b33b8e0a93e
Fatai Akinyemi <ojuotimi04@gmail.com>
2024-04-18 07:49:14 +0100

    pages commit

---
+++ src/router.jsx
@@ -0,0 +1,38 @@
+import { createBrowserRouter } from "react-router-dom";
+import Home from "./pages/Home";
+import MainLayout from "./layout/MainLayout";
+import About from "./pages/About";
+import Project from "./pages/Project";
+import Contact from "./pages/Contact";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <MainLayout />,
+
+    children: [
+      {
+        index: true,
+        element: <Home />,
+      },
+      {
+        path: "/about",
+        element: <About />,
+      },
+      {
+        path: "/skills",
+        element: <Skills />,
+      },
+      {
+        path: "/project",
+        element: <Project />,
+      },
+      {
+        path: "/contact",
+        element: <Contact />,
+      },
+    ],
+  },
+]);
+
+export default router;
