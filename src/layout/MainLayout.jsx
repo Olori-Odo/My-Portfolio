fa309deac48a862c5686ddfaac4ba3b548cf3fa7
Fatai Akinyemi <ojuotimi04@gmail.com>
2024-04-19 06:44:01 +0100

    navbar update

--- src/layout/MainLayout.jsx
+++ src/layout/MainLayout.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
+import NavBar from "../component/navigation/navbar/NavBar";
 
 const MainLayout = () => {
   return (
     <div id="layout">
+      <NavBar />
       <div id="display">
         <Outlet />
       </div>
