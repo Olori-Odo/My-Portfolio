bae4c23e75eefb5a5f66c93eeb4cf631d2880fe8
Fatai Akinyemi <ojuotimi04@gmail.com>
2024-04-21 17:42:35 +0100

    commit

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,14 @@ import App from "./App.jsx";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  // <React.StrictMode>
-  <App />
-  // </React.StrictMode>,
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 );
+
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+//   document.getElementById("root")
+// );
