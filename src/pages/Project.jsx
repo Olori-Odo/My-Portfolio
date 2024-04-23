e7eb834bbc19c0649715b709463d73e522d192b9
Fatai Akinyemi <ojuotimi04@gmail.com>
2024-04-23 15:05:35 +0100

    add

--- src/pages/Project.jsx
+++ src/pages/Project.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const Project = () => {
-  return <div>Project</div>;
+  return (
+    <>
+      <div id="projects"></div>
+    </>
+  );
 };
 
 export default Project;
