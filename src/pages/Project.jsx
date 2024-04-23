97724841f0749b58e7dd18814ea00eb7815d7846
Fatai Akinyemi <ojuotimi04@gmail.com>
2024-04-23 16:04:55 +0100

    update

--- src/pages/Project.jsx
+++ src/pages/Project.jsx
@@ -1,9 +1,19 @@
 import React from "react";
+import "./Project.css";
+import Food from "../assets/food.jpg";
+import CRM from "../assets/crm.png";
 
 const Project = () => {
   return (
     <>
-      <div id="projects"></div>
+      <div>
+        <h1 id="p-header">Projects..</h1>
+
+        <div id="projects">
+          <img src={Food} alt="recipe" />
+          <img src={CRM} alt="ticketing" />
+        </div>
+      </div>
     </>
   );
 };
