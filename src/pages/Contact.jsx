6796d3c2021685e579d387701a8edb13cded03d0
Fatai Akinyemi <ojuotimi04@gmail.com>
2024-05-08 16:36:37 +0100

    update

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Contact.css";
 import { Link } from "react-router-dom";
 import { FaGithub } from "react-icons/fa";
@@ -6,6 +6,28 @@ import { IoLogoLinkedin } from "react-icons/io";
 import { FaXTwitter } from "react-icons/fa6";
 
 const Contact = () => {
+  const [projectOffer, setProjectOffer] = useState({
+    name: "",
+    email: "",
+    subject: "",
+    message: "",
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+
+    setProjectOffer((prev) => ({
+      ...prev,
+      [name]: value,
+    }));
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    console.log(projectOffer);
+  };
+
   return (
     <div id="contact-page">
       <div id="contact-info">
@@ -35,21 +57,40 @@ const Contact = () => {
       </div>
 
       <div id="form">
-        <form action="">
+        <form action="/contact" method="post" onSubmit={handleSubmit}>
           <label htmlFor="name">Name</label> <br />
-          <input type="text" id="name" />
+          <input
+            type="text"
+            value={projectOffer.name}
+            onChange={handleChange}
+            id="name"
+          />
           <br />
-          <label id="email" htmlFor="email">
-            Email
-          </label>{" "}
-          <br />
-          <input id="email" type="email" />
+          <label htmlFor="email">Email</label> <br />
+          <input
+            id="email"
+            value={projectOffer.email}
+            onChange={handleChange}
+            type="email"
+          />
           <br />
           <label htmlFor="subject">Subject</label> <br />
-          <input type="text" id="subject" />
+          <input
+            type="text"
+            value={projectOffer.subject}
+            onChange={handleChange}
+            id="subject"
+          />
           <br />
           <label htmlFor="message">Message</label> <br />
-          <textarea name="message" id="message" cols="30" rows="10"></textarea>
+          <textarea
+            name="message"
+            value={projectOffer.message}
+            onChange={handleChange}
+            id="message"
+            cols="30"
+            rows="10"
+          ></textarea>
           <br />
           <button type="submit">Submit</button>
         </form>
