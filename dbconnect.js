7ed98018b97adb8ae434e56fafae3380eb16e138
Fatai Akinyemi <ojuotimi04@gmail.com>
2024-06-05 22:57:48 +0100

    resume update

--- dbconnect.js
+++ dbconnect.js
@@ -1,10 +1,10 @@
 const mongoose = require("mongoose");
 require("dotenv").config();
 
-mongoose
-  .connect(
-    "mongodb+srv://ojuotimi04:ojuotimi04@testmongodb.lr601dn.mongodb.net/project"
-  )
+mongoose.connect(
+  "mongodb+srv://ojuotimi04:ojuotimi04@testmongodb.lr601dn.mongodb.net/project"
+);
+connect
   .then(() => {
     console.log("Database connected");
   })
--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import "./App.css";
 import { RouterProvider } from "react-router-dom";
 import router from "./router";
+import NavBar from "./component/navigation/navbar/NavBar";
 
 function App() {
   return (
